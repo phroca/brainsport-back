2f5a118b2309e132b18d082e33c035cdd3e4db8e
ROCA Philippe <roca.philippe63@gmail.com>
2024-04-29 10:06:43 +0200

    BS-B : update cors policy$

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var userDataCard= require('./routes/userdatacard');
+var userDataCard = require('./routes/userdatacard');
 var userFamillyProgress = require('./routes/userfamillyprogress');
 var userFriends = require('./routes/userfriends');
 var userPlayHistory = require('./routes/userplayhistory');
@@ -20,12 +20,12 @@ var rewardsData = require('./routes/rewardsData');
 var app = express();
 
 //CORS Middleware
-app.use(function(req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,24 +39,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/userdatacard',userDataCard );
-app.use('/userfamillyprogress',userFamillyProgress );
-app.use('/userfriends' , userFriends);
-app.use('/userplayhistory',userPlayHistory);
-app.use('/userstepperdata',userStepperData);
-app.use('/communitychallenge',communityChallenge);
-app.use('/communityevent',communityEvent);
-app.use('/communitygroup',communityGroup);
-app.use('/progresspointdata',progressPointData);
-app.use('/rewardsdata',rewardsData);
+app.use('/userdatacard', userDataCard);
+app.use('/userfamillyprogress', userFamillyProgress);
+app.use('/userfriends', userFriends);
+app.use('/userplayhistory', userPlayHistory);
+app.use('/userstepperdata', userStepperData);
+app.use('/communitychallenge', communityChallenge);
+app.use('/communityevent', communityEvent);
+app.use('/communitygroup', communityGroup);
+app.use('/progresspointdata', progressPointData);
+app.use('/rewardsdata', rewardsData);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
